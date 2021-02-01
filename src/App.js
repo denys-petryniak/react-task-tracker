8430744056d68b4ee90f9b99e02c870a246a3206
Denys Petryniak <denys.petryniak@gmail.com>
2021-02-02 00:24:07 +0200

    added base button & tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Doctor',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true
+		},
+		{
+			id: 2,
+			text: 'Meeting at University',
+			day: 'Feb 6th at 2:30pm',
+			reminder: true
+		},
+		{
+			id: 3,
+			text: 'Shopping',
+			day: 'Feb 7th at 2:30pm',
+			reminder: false
+		}
+	])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
